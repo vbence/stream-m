5488681234b8198d1032e95c6d906a3d8343fef6
Varga Bence <vbence@czentral.org>
2014-09-15 17:14:05 +0200

    MP4 header generation hooked into RTMP publisher app.

--- src/org/czentral/incubator/streamm/PublisherAppInstance.java
+++ src/org/czentral/incubator/streamm/PublisherAppInstance.java
@@ -20,8 +20,17 @@
 package org.czentral.incubator.streamm;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
+import org.czentral.data.binary.AnnotationMapSerializationContext;
+import org.czentral.data.binary.BitBuffer;
+import org.czentral.data.binary.SerializerFactoryImpl;
+import org.czentral.data.binary.serializer.DescriptorInteger;
+import org.czentral.data.binary.serializer.FixedInteger;
+import org.czentral.data.binary.serializer.FixedString;
+import org.czentral.data.binary.serializer.GeneralSerializer;
+import org.czentral.minihttp.HTTPException;
 import org.czentral.minirtmp.AMFPacket;
 import org.czentral.minirtmp.ApplicationContext;
 import org.czentral.minirtmp.MessageInfo;
@@ -34,17 +43,25 @@ import org.czentral.minirtmp.RTMPCommand;
  */
 class PublisherAppInstance implements ApplicationInstance {
     
+    private static final String HANDLER_NAME_SET_DATA_FRAME = "@setDataFrame";
+    
     protected ApplicationContext context;
 
     protected Properties props;
 
     protected Map<String, ControlledStream> streams;
     
+    protected String streamID;
+    
     protected double fcpublishTxID;
     
-    protected byte[] videoMeta;
+    protected Map<String, Object> metaData;
     
-    protected byte[] audioMeta;
+    protected Mp4Header header = new Mp4Header();
+    
+    protected Map<Integer, Integer> streamToTrack = new HashMap<>();
+    
+    protected boolean streamStarted = false;
 
     
     public PublisherAppInstance(Properties props, Map<String, ControlledStream> streams) {
@@ -60,7 +77,7 @@ class PublisherAppInstance implements ApplicationInstance {
 
     @Override
     public void invokeCommand(MessageInfo mi, RTMPCommand command) {
-        System.err.println("called: " + command.getName());
+        System.err.println("cmd: " + command.getName() + " " + command.getArguments());
         
         if (command.getName().equals("connect")) {
             cmdConnect(mi, command);
@@ -68,6 +85,9 @@ class PublisherAppInstance implements ApplicationInstance {
         } else if (command.getName().equals("releaseStream")) {
             sendSuccess(mi, command);
         
+        } else if (command.getName().equals("FCUnpublish")) {
+            sendSuccess(mi, command);
+            
         } else if (command.getName().equals("createStream")) {
             cmdCreateStream(mi, command);
         
@@ -79,14 +99,17 @@ class PublisherAppInstance implements ApplicationInstance {
         
         } else {
             System.err.println("Unknown command: " + command.getName());
-            
-            AMFPacket response = new AMFPacket();
-            response.writeString("_error");
-            response.writeNumber(command.getTxid());
-            response.writeMixed(null);
-            response.writeMixed(null);
-            context.writeCommand(mi.streamID, response);
+            sendError(mi, command, null, null);
         }
+    }
+    
+    protected void sendError(MessageInfo mi, RTMPCommand command, Object object, Object information) {
+        AMFPacket response = new AMFPacket();
+        response.writeString("_error");
+        response.writeNumber(command.getTxid());
+        response.writeMixed(null);
+        response.writeMixed(null);
+        context.writeCommand(mi.streamID, response);
     }
     
     protected void sendSuccess(MessageInfo mi, RTMPCommand command) {
@@ -132,6 +155,34 @@ class PublisherAppInstance implements ApplicationInstance {
     }
 
     protected void cmdFCPublish(MessageInfo mi, RTMPCommand command) {
+        
+        String streamName = (String)command.getArguments().get(0);
+        int separatorPos = streamName.indexOf(';');
+        if (separatorPos == -1) {
+            throw new RuntimeException("No separator found in stream name.");
+        }
+        String clientStreamID = streamName.substring(0, separatorPos);
+        String clientSecret = streamName.substring(separatorPos + 1);
+
+        String streamSectret = props.getProperty("streams." + clientStreamID + ".password");
+        if (streamSectret == null) {
+            Map<String, Object> info = new HashMap<>();
+            info.put("code", "NetStream.Publish.BadName");  // with lack of better choice
+            info.put("level", "error");
+            info.put("description", "Stream does not exist.");
+            sendError(mi, command, null, info);
+            throw new RuntimeException("Stream does not exist.");
+        }
+        if (!clientSecret.equals(streamSectret)) {
+            Map<String, Object> info = new HashMap<>();
+            info.put("code", "NetStream.Publish.BadName");  // with lack of better choice
+            info.put("level", "error");
+            info.put("description", "Authentication error.");
+            sendError(mi, command, null, info);
+            throw new RuntimeException("Authentication error.");
+        }
+        
+        streamID = clientStreamID;
         fcpublishTxID = command.getTxid();
         sendSuccess(mi, command);
     }
@@ -143,7 +194,7 @@ class PublisherAppInstance implements ApplicationInstance {
 
         response.writeMixed(null);
 
-        Map<String, Object> arg2 = new HashMap<String, Object>();
+        Map<String, Object> arg2 = new HashMap<>();
         arg2.put("level", "status");
         arg2.put("code", "NetStream.Publish.Start");
         arg2.put("description", "");
@@ -157,19 +208,92 @@ class PublisherAppInstance implements ApplicationInstance {
     
     @Override
     public void onData(MessageInfo mi, RTMPCommand command) {
-        System.err.println(command.getArguments());
+        String handlerName = (String)command.getArguments().get(0);
+        if (HANDLER_NAME_SET_DATA_FRAME.equals(handlerName)) {
+            metaData = (Map)command.getArguments().get(2);
+            System.err.println("metadata: " + command.getArguments());
+        } else {
+            System.err.println("Unknown data: " + command.getArguments());
+        }
     }
 
     @Override
     public void mediaChunk(MessageInfo mi, byte[] readBuffer, int payloadOffset, int payloadLength) {
-        if (videoMeta == null && mi.type == 0x09) {
-            videoMeta = new byte[payloadLength];
-            System.arraycopy(readBuffer, payloadOffset, videoMeta, 0, payloadLength);
+        Integer trackID = streamToTrack.get(mi.streamID);
+        
+        if (trackID == null) {
+            
+            if (mi.type == 0x09) {
+                final int SKIP_BYTES = 5;
+                byte[] decoderSpecificBytes = new byte[payloadLength - SKIP_BYTES];
+                System.arraycopy(readBuffer, payloadOffset + SKIP_BYTES, decoderSpecificBytes, 0, payloadLength - SKIP_BYTES);
+                int timescale = metaData.get("framerate") != null
+                        ? ((Double)metaData.get("framerate")).intValue() / 1000
+                        : 30;
+                int newTrackID = header.addVideoTrack(((Double)metaData.get("width")).intValue()
+                        , ((Double)metaData.get("height")).intValue()
+                        , timescale
+                        , decoderSpecificBytes);
+                streamToTrack.put(mi.streamID, newTrackID);
+                
+            } else if (mi.type == 0x08) {
+                final int SKIP_BYTES = 2;
+                byte[] decoderSpecificBytes = new byte[payloadLength - SKIP_BYTES];
+                System.arraycopy(readBuffer, payloadOffset + SKIP_BYTES, decoderSpecificBytes, 0, payloadLength - SKIP_BYTES);
+                int newTrackID = header.addAudioTrack(((Double)metaData.get("audiosamplerate")).intValue()
+                        , ((Double)metaData.get("audiosamplesize")).intValue()
+                        , ((Double)metaData.get("audiodatarate")).intValue()
+                        , decoderSpecificBytes);
+                streamToTrack.put(mi.streamID, newTrackID);
+            }
+            
+        } else {
+            
+            if (!streamStarted) {
+                startStream();
+            }
+            
         }
-        if (audioMeta == null && mi.type == 0x08) {
-            audioMeta = new byte[payloadLength];
-            System.arraycopy(readBuffer, payloadOffset, audioMeta, 0, payloadLength);
+        //HexDump.displayHex(readBuffer, payloadOffset, Math.min(payloadLength, 32));
+    }
+    
+    private void startStream() {
+        streamStarted = true;
+        
+        ControlledStream stream = streams.get(streamID);
+        if (stream != null) {
+            stream.stop();
         }
+        
+        stream = new ControlledStream(Integer.parseInt(props.getProperty("streams." + streamID + ".limit")));
+        stream.setMimeType("video/mp4");
+        streams.put(streamID, stream);
+        
+        // rendering mp4 header
+        
+        final int BUFFER_LENGTH = 32 * 1024;
+        BitBuffer bb = new BitBuffer(new byte[BUFFER_LENGTH], 0, BUFFER_LENGTH * 8);
+        SerializerFactoryImpl sf = new SerializerFactoryImpl();
+        sf.addSerializer(int.class, new FixedInteger());
+        sf.addSerializer(Integer.class, new FixedInteger());
+        sf.addSerializer(Number.class, new FixedInteger());
+        sf.addSerializer(String.class, new FixedString());
+        sf.addSerializer(DescriptorInteger.class, new DescriptorInteger());
+        GeneralSerializer s = new GeneralSerializer(sf);
+        AnnotationMapSerializationContext ctx = new AnnotationMapSerializationContext(bb, null, s);
+        
+        s.serialize(header.getFileType(), ctx);
+        s.serialize(header.getMovie(), ctx);
+        
+        if ((bb.getBitPosition() & 0x07) != 0) {
+            throw new RuntimeException("Rendered header is not byte-aligned.");
+        }
+        int headerLength = (int)(bb.getBitPosition() >> 3);
+        byte[] headerBytes = new byte[headerLength];
+        System.arraycopy(bb.getBuffer(), 0, headerBytes, 0, headerLength);
+        
+        stream.setHeader(headerBytes);
+        
     }
     
 }
