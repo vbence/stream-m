ff126eec82421bb0c43aba01c6f3c16e89c3e254
Bence Varga <vbence@czentral.org>
2018-11-20 22:36:23 +0100

    MIT License.

--- src/main/java/org/czentral/incubator/streamm/Bootstrap.java
+++ src/main/java/org/czentral/incubator/streamm/Bootstrap.java
@@ -1,21 +1,19 @@
 /*
-    This file is part of "stream.m" software, a video broadcasting tool
-    compatible with Google's WebM format.
-    Copyright (C) 2011 Varga Bence
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright 2018 Bence Varga
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package org.czentral.incubator.streamm;
 
 import org.czentral.incubator.streamm.web.SnapshotResource;
@@ -26,10 +24,8 @@ import java.io.*;
 import java.util.*;
 import org.czentral.minihttp.FileResource;
 import org.czentral.minihttp.*;
-import org.czentral.minirtmp.ApplicationLibrary;
 import org.czentral.minirtmp.ApplicationLibraryImpl;
 import org.czentral.minirtmp.MiniRTMP;
-import org.czentral.minirtmp.ApplicationInstance;
 
 public class Bootstrap {
     
